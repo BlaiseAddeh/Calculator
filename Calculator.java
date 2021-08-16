5c96307a75b5894324d280bc470e70344349ce10
ADDEH BLAISE <b.addeh@my-kiwi.fr>
2021-08-16 20:13:44 +0200

    Ajout des fonctions de soustraction

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,17 @@ public class Calculator {
    public int addition(int a, int b) {
        return a + b;
    }
+
+  public int addition(double a, double b) {
+       return b + a;
+   }
+
+    public int substraction(int a, int b) {
+       return a - b;
+   }
+
+  public int substraction(double a, double b) {
+       return b - a;
+   }
+
 }
